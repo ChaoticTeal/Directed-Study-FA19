0e732f137efe7f6c4959fc0193e6b52d27d046fb
PurpleNinjaLink <jawhittom@gmail.com>
2019-09-16 18:32:01 -0500

    Implements Directional NPC Animations
    Adds directional idle animations for the NPCs, which allow them to face the player when talking.

---
+++ Assets/Scripts/AnimateNPC.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateNPC : MonoBehaviour
+{
+    /// <summary>
+    /// The NPC's Animator component
+    /// </summary>
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    /// <summary>
+    /// Sets the NPC's animation direction to face the player initiating the conversation
+    /// </summary>
+    /// <param name="playerTransform">The location of the player.</param>
+    public void SetAnimationDirectionOnConversationStart(Transform playerTransform)
+    {
+        Vector2 playerDirection = GetOneDimensionalDirectionalVector(playerTransform.position);
+        if (playerDirection == Vector2.down)
+            animator.SetTrigger("Move Down");
+        else if (playerDirection == Vector2.up)
+            animator.SetTrigger("Move Up");
+        else if (playerDirection == Vector2.left)
+            animator.SetTrigger("Move Left");
+        else if (playerDirection == Vector2.right)
+            animator.SetTrigger("Move Right");
+
+    }
+
+    /// <summary>
+    /// Creates a normalized directional vector from the distance between the NPC and player
+    /// </summary>
+    private Vector2 GetOneDimensionalDirectionalVector(Vector2 playerPosition)
+    {
+        Vector2 normalizedPlayerDirection = playerPosition - (Vector2)transform.position;
+        if (Mathf.Abs(normalizedPlayerDirection.x) >= Mathf.Abs(normalizedPlayerDirection.y))
+            normalizedPlayerDirection.y = 0;
+        else
+            normalizedPlayerDirection.x = 0;
+        return normalizedPlayerDirection.normalized;
+    }
+}
