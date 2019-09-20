8281cf34a9f0825cf88c93d4a38fdf5c43d9ec47
PurpleNinjaLink <jawhittom@gmail.com>
2019-09-19 20:00:37 -0500

    Installs Cinemachine
    Installs Cinemachine and sternly encourages it to cooperate with the Pixel Perfect camera.

---
+++ Assets/Scripts/CinemachinePixelPerfect.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinemachinePixelPerfect : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Cinemachine.CinemachineVirtualCamera vCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = FindObjectOfType<Cinemachine.CinemachineBrain>().
+            gameObject.GetComponent<Camera>();
+        vCam = GetComponent<Cinemachine.CinemachineVirtualCamera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vCam.m_Lens.OrthographicSize != mainCamera.orthographicSize)
+            vCam.m_Lens.OrthographicSize = mainCamera.orthographicSize;
+    }
+}
