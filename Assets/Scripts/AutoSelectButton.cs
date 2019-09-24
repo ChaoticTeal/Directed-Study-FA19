f29953177af03cf9b71f87e82d9c1cb2da888d9c
PurpleNinjaLink <jawhittom@gmail.com>
2019-09-23 20:35:35 -0500

    Adds Audio, Forces Continue Button
    Forces dialogue to wait for a continue input before closing, which solves the issue of the sign text not displaying for long enough. Also adds audio effects for the doors and switches, but they go unused at present.

---
+++ Assets/Scripts/AutoSelectButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoSelectButton : MonoBehaviour
+{
+    /// <summary>
+    /// The Selectable component of this GameObject
+    /// </summary>
+    private UnityEngine.UI.Selectable selectable;
+
+    private UnityEngine.EventSystems.EventSystem eventSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        eventSystem = GameObject.FindObjectOfType<UnityEngine.EventSystems.EventSystem>();
+        selectable = GetComponent<UnityEngine.UI.Selectable>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (eventSystem.currentSelectedGameObject == null)
+            selectable.Select();
+    }
+}
